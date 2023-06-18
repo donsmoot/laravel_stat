50a49003395a75e985f744d4efb8f04cf9414852
donsmoot <delopsttp@gmail.com>
2023-06-18 18:26:08 +0300

    Готовый шаблон со всей инфой

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -20,9 +20,7 @@
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
         <img class="animation__wobble" src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
-
     </div>
-    @include('includes/sidebar')
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-dark">
         <!-- Left navbar links -->
@@ -163,7 +161,6 @@
 
     <!-- Main Sidebar Container -->
     @include('includes/sidebar')
-
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
